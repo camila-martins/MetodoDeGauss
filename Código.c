3b3e9885790468bee66e28da6af8c123ff7b909a
Camila Martins <37345577+camila-martins@users.noreply.github.com>
2018-04-08 16:48:33 -0300

    Update Código.c

--- Código.c
+++ Código.c
@@ -3,7 +3,32 @@
 #include<math.h>
 #include<stdlib.h>
 
-
+FILE *fp;
+double **ler(char *matriz, int *dim)
+{
+  int i, j, a;
+  double **M;
+  fp=fopen("Matriz.dat", "r");
+  i=fscanf(fp, "%d\t", dim);
+  M=malloc(*dim*sizeof(double*));
+  for(i=0; i<*dim; i++)
+  {
+    M[i]=malloc((*dim+1)*sizeof(double));
+  }
+  i=j=0;
+  
+  while(fscanf(fp, "%d\t", &a)!=EOF)
+  {
+    M[i][j]=a;
+    j++;
+    if(j==*dim+1)
+    {
+      j=0;
+      j++;
+    }
+  }
+  return (M);
+}
 
 int main (int argc , char ** argv )
 {
